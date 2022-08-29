08135382d84dc3e6ac8660755fb440a25f8ef11b
ArmandoSaenz <43352833+ArmandoSaenz@users.noreply.github.com>
2022-08-29 11:39:51 -0500

    Agregar archivos de proyecto.

---
+++ Tanque_de_Agua/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Tanque_de_Agua
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.plotView1 = new OxyPlot.WindowsForms.PlotView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbRadio = new System.Windows.Forms.TextBox();
+            this.tbF1 = new System.Windows.Forms.TextBox();
+            this.tbF2 = new System.Windows.Forms.TextBox();
+            this.tbH0 = new System.Windows.Forms.TextBox();
+            this.btnGraficar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.plotView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(496, 290);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Grafica";
+            // 
+            // plotView1
+            // 
+            this.plotView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.plotView1.Location = new System.Drawing.Point(3, 16);
+            this.plotView1.Name = "plotView1";
+            this.plotView1.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView1.Size = new System.Drawing.Size(490, 271);
+            this.plotView1.TabIndex = 0;
+            this.plotView1.Text = "plotView1";
+            this.plotView1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnGraficar);
+            this.groupBox2.Controls.Add(this.tbH0);
+            this.groupBox2.Controls.Add(this.tbF2);
+            this.groupBox2.Controls.Add(this.tbF1);
+            this.groupBox2.Controls.Add(this.tbRadio);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(514, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(235, 287);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Parametros";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Radio del tanque:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Flujo de entrada:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Flujo de salida:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Altura Inicial:";
+            // 
+            // tbRadio
+            // 
+            this.tbRadio.Location = new System.Drawing.Point(106, 20);
+            this.tbRadio.Name = "tbRadio";
+            this.tbRadio.Size = new System.Drawing.Size(100, 20);
+            this.tbRadio.TabIndex = 4;
+            // 
+            // tbF1
+            // 
+            this.tbF1.Location = new System.Drawing.Point(106, 49);
+            this.tbF1.Name = "tbF1";
+            this.tbF1.Size = new System.Drawing.Size(100, 20);
+            this.tbF1.TabIndex = 5;
+            // 
+            // tbF2
+            // 
+            this.tbF2.Location = new System.Drawing.Point(106, 81);
+            this.tbF2.Name = "tbF2";
+            this.tbF2.Size = new System.Drawing.Size(100, 20);
+            this.tbF2.TabIndex = 6;
+            // 
+            // tbH0
+            // 
+            this.tbH0.Location = new System.Drawing.Point(106, 113);
+            this.tbH0.Name = "tbH0";
+            this.tbH0.Size = new System.Drawing.Size(100, 20);
+            this.tbH0.TabIndex = 7;
+            // 
+            // btnGraficar
+            // 
+            this.btnGraficar.Location = new System.Drawing.Point(11, 158);
+            this.btnGraficar.Name = "btnGraficar";
+            this.btnGraficar.Size = new System.Drawing.Size(75, 23);
+            this.btnGraficar.TabIndex = 8;
+            this.btnGraficar.Text = "Graficar";
+            this.btnGraficar.UseVisualStyleBackColor = true;
+            this.btnGraficar.Click += new System.EventHandler(this.btnGraficar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 314);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private OxyPlot.WindowsForms.PlotView plotView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnGraficar;
+        private System.Windows.Forms.TextBox tbH0;
+        private System.Windows.Forms.TextBox tbF2;
+        private System.Windows.Forms.TextBox tbF1;
+        private System.Windows.Forms.TextBox tbRadio;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Tanque_de_Agua/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Oxyplot Libraries
+using OxyPlot;
+using OxyPlot.Series;
+
+namespace Tanque_de_Agua
+{
+    public partial class Form1 : Form
+    {
+        //Local variables
+        double A, r, h0, f1, f2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private double h(double t)
+        {
+            return (1 / A) * (f1 - f2) * t + h0;
+        }
+        private void btnGraficar_Click(object sender, EventArgs e)
+        {
+            r = double.Parse(this.tbRadio.Text);
+            f1 = double.Parse(this.tbF1.Text);
+            f2 = double.Parse(this.tbF2.Text);
+            h0 = double.Parse(this.tbH0.Text);
+            A = Math.PI * Math.Pow(r, 2);
+            PlotModel Modelo = new PlotModel();
+            Modelo.Series.Add(new FunctionSeries(this.h, 0, 600, 0.1));
+            plotView1.Model = Modelo;
+        }
+    }
+}
